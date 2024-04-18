cbea655ac2f9618e88e8500b8162a5dea46e5253
blacksmith. <101953303+bblacksmithh@users.noreply.github.com>
2024-04-18 08:36:58 +0200

    front end added again

---
+++ Front End/Front End/src/app/login/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import LandingNavbar from "@/components/LandingNavbar";
+import LoginContent from "@/components/LoginContent";
+
+export default function Login() {
+
+    return (
+        <main>
+            <LandingNavbar/>
+            <LoginContent/>
+        </main>
+    );
+}
